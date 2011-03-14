0a5a55877bc761947a084e2a5571cb038a130e05
Flow86 <floriand70@gmail.com>
2011-03-14 13:38:19 +0100

    initial

---
+++ make.bat
@@ -0,0 +1,1 @@
+"c:\Program Files (x86)\Java\jdk1.6.0_24\bin\javac.exe" -classpath %APPDATA%\.minecraft\bin\minecraft.jar mod_WeakRedstone.java
---
+++ mod_WeakRedstone.java
@@ -0,0 +1,13 @@
+public class mod_WeakRedstone extends BaseMod
+{
+	public mod_WeakRedstone()
+	{
+		qk.aN.c(1.0F);
+		qk.aO.c(1.0F);
+	}
+	
+	public String Version()
+	{
+		return "1.3_01";
+	}
+}
